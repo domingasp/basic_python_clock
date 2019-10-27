2795eb62ee707b507e280e26193145b93bc7b89d
Domingas <domingas@Domingass-MacBook-Pro.local>
2019-10-27 17:19:57 +0000

    Added hour, minute and second hands to the circle clock. These move depending on the current time. Hours are converted to 12 hour time to reflect the face of a clock.

--- clock.py
+++ clock.py
@@ -1,4 +1,5 @@
 ############ IMPORTS ############
+import math
 import datetime, time
 import tkinter as tk
 
@@ -14,20 +15,19 @@ class GUI:
         self.rectangeCanvas = tk.Canvas(self.master, width = 360, height = 58)
         self.rectangeCanvas.pack()
 
-        # Create the hour rectangle (x1 = beginning position, y1 = beginning position, x2 = current time, y2 = end position controls the height)
+        # Create the hour, minute and second rectangle
         self.hourRectangle = self.rectangeCanvas.create_rectangle(3, 3, 0, 18, fill = 'green')
-
-        # Create the minute rectangle (x1 = beginning position, y1 = beginning position, x2 = current time, y2 = end position controls the height)
         self.minuteRectangle = self.rectangeCanvas.create_rectangle(3, 23, 0, 38, fill = 'blue')
-
-        # Create the second rectangle (x1 = beginning position, y1 = beginning position, x2 = current time, y2 = end position controls the height)
-        self.secondRectangle = self.rectangeCanvas.create_rectangle(3, 43, 0, 58, fill = 'yellow')
+        self.secondRectangle = self.rectangeCanvas.create_rectangle(3, 43, 0, 58, fill = 'magenta')
 
         # Canvas for the circle clock
         self.circleCanvas = tk.Canvas(self.master, width = 360, height = 360)
         self.circleCanvas.pack()
 
-        self.secondHand = self.circleCanvas.create_rectangle(180, 180, 180, 0)
+        # Create the hour, minute and second hands
+        self.hourHand = self.circleCanvas.create_line(180, 180, 0, 90, fill = 'green')
+        self.minuteHand = self.circleCanvas.create_line(180, 180, 0, 140, fill = 'blue')
+        self.secondHand = self.circleCanvas.create_line(180, 180, 0, 180, fill = 'magenta')
 
 # Called to get the current time and alter the gui to reflect the time
 def getCurrentTime(root, gui):
@@ -41,6 +41,27 @@ def getCurrentTime(root, gui):
     gui.rectangeCanvas.coords(gui.hourRectangle, 3, 3, 0 + (hour * 15), 18)
     gui.rectangeCanvas.coords(gui.minuteRectangle, 3, 23, 0 + (minute * 6), 38)
     gui.rectangeCanvas.coords(gui.secondRectangle, 3, 43, 0 + (second * 6), 58)
+
+    # Calculates the seconds hand angle and updates the GUI to reflect the new time
+    hour_angle_in_radians = ((hour % 12) * 30 - 90) * math.pi / 180
+    hour_line_length = 90
+    hour_end_x = 180 + hour_line_length * math.cos(hour_angle_in_radians)
+    hour_end_y = 180 + hour_line_length * math.sin(hour_angle_in_radians)
+    gui.circleCanvas.coords(gui.hourHand, 180, 180, hour_end_x, hour_end_y)
+
+    # Calculates the seconds hand angle and updates the GUI to reflect the new time
+    minute_angle_in_radians = (minute * 6 - 90) * math.pi / 180
+    minute_line_length = 140
+    minute_end_x = 180 + minute_line_length * math.cos(minute_angle_in_radians)
+    minute_end_y = 180 + minute_line_length * math.sin(minute_angle_in_radians)
+    gui.circleCanvas.coords(gui.minuteHand, 180, 180, minute_end_x, minute_end_y)
+
+    # Calculates the seconds hand angle and updates the GUI to reflect the new time
+    second_angle_in_radians = (second * 6 - 90) * math.pi / 180
+    second_line_length = 180
+    second_end_x = 180 + second_line_length * math.cos(second_angle_in_radians)
+    second_end_y = 180 + second_line_length * math.sin(second_angle_in_radians)
+    gui.circleCanvas.coords(gui.secondHand, 180, 180, second_end_x, second_end_y)
 
     # Print the current time in Hours:Minutes:Seconds
     print(str(hour) + ':' + str(minute) + ':' + str(second))
